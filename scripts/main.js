76f6e6fdfd9aee5f28ea042bd527e576bb741347
Ling Ma <lma1169949842@gmail.com>
2019-02-28 17:48:50 -0600

    pic

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
-var heading=document.querySelector('h1');
-heading.textContent='Hello!';
+var contactForm=document.querySelector("form");
+
+var mail = document.getElementById('mail');
+var msg = document.getElementById('msg');
+// var submit = document.getElementById('submit');
+
+
+contactForm.onsubmit = function(e) {
+  if (mail.value === '' || msg.value === '') {
+    e.preventDefault();
+    alert("Mail and Message required!");
+  }
+}
+
+
+var bg=document.querySelector("div");
+// var WIDTH=window.innerWidth;
+// var HEIGHT=window.innerHeight;
+// bg.style.width=WIDTH+'px';
+// bg.style.height=HEIGHT+'px';
+// bg.style.backgroundColor='black';
